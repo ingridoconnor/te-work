627247db8f550d0bb798983be817c29e18914a3f
Ingrid OConnor <ingrido@bgsu.edu>
2020-06-01 14:38:42 -0400

    convert_num_bt_1_100_to_string

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -4,7 +4,7 @@ public class KataFizzBuzz {
 	
 	private int myInt;
 
-	public String fizzBuzz(int myInt) {
+	public String fizzBuzz(Integer myInt) {
 		this.myInt = myInt;
 		if(myInt % 3 == 0 && myInt % 5 == 0) {
 			return "FizzBuzz";
@@ -15,6 +15,9 @@ public class KataFizzBuzz {
 		}
 		if(myInt % 5 == 0) {
 			return "Buzz";
+		}
+		if(myInt >= 1 && myInt <= 100) {
+			return myInt.toString();
 		}
 		
 		return "";
